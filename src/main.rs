7af7589807f6948efadccad443cf7f0e1188d388
andriyDev <andriydzikh@gmail.com>
2024-10-19 14:46:02 -0700

    Keep track of all the modes reported by heads.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use wayland_client::{event_created_child, protocol::wl_registry, Connection, Dispatch};
+use std::collections::HashMap;
+
+use wayland_client::{
+    backend::ObjectId,
+    event_created_child,
+    protocol::wl_registry::{self, WlRegistry},
+    Connection, Dispatch, Proxy,
+};
 use wayland_protocols_wlr::output_management::v1::client::{
-    zwlr_output_head_v1, zwlr_output_manager_v1, zwlr_output_mode_v1::ZwlrOutputModeV1,
+    zwlr_output_head_v1::{self, ZwlrOutputHeadV1},
+    zwlr_output_manager_v1::{self, ZwlrOutputManagerV1},
+    zwlr_output_mode_v1::{self, ZwlrOutputModeV1},
 };
 
 fn main() {
@@ -12,18 +21,49 @@ fn main() {
 
     display.get_registry(&qhandle, ());
 
-    let mut app_data = AppData;
+    let mut app_data = AppData::default();
     loop {
         event_queue.blocking_dispatch(&mut app_data).unwrap();
     }
 }
 
-struct AppData;
+#[derive(Default)]
+struct AppData {
+    id_to_partial_mode: HashMap<ObjectId, PartialMode>,
+    id_to_mode: HashMap<ObjectId, Mode>,
+}
 
-impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
+#[derive(Clone, Copy, Debug, Default)]
+struct PartialMode {
+    size: Option<(u32, u32)>,
+    refresh: Option<u32>,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Mode {
+    size: (u32, u32),
+    refresh: Option<u32>,
+}
+
+impl TryFrom<PartialMode> for Mode {
+    // TODO: Make an actual error type.
+    type Error = ();
+
+    fn try_from(value: PartialMode) -> Result<Self, Self::Error> {
+        let Some(size) = value.size else {
+            return Err(());
+        };
+        Ok(Self {
+            size,
+            refresh: value.refresh,
+        })
+    }
+}
+
+impl Dispatch<WlRegistry, ()> for AppData {
     fn event(
         _state: &mut Self,
-        proxy: &wl_registry::WlRegistry,
+        proxy: &WlRegistry,
         event: wl_registry::Event,
         _data: &(),
         _conn: &Connection,
@@ -50,10 +90,10 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
     }
 }
 
-impl Dispatch<zwlr_output_manager_v1::ZwlrOutputManagerV1, ()> for AppData {
+impl Dispatch<ZwlrOutputManagerV1, ()> for AppData {
     fn event(
-        _state: &mut Self,
-        _proxy: &zwlr_output_manager_v1::ZwlrOutputManagerV1,
+        state: &mut Self,
+        _proxy: &ZwlrOutputManagerV1,
         event: zwlr_output_manager_v1::Event,
         _data: &(),
         _conn: &Connection,
@@ -62,40 +102,81 @@ impl Dispatch<zwlr_output_manager_v1::ZwlrOutputManagerV1, ()> for AppData {
         let zwlr_output_manager_v1::Event::Done { .. } = event else {
             return;
         };
+        for (id, partial_mode) in state.id_to_partial_mode.drain() {
+            state.id_to_mode.insert(
+                id,
+                partial_mode
+                    .try_into()
+                    .expect("Done is called, so the partial mode should be well-defined"),
+            );
+        }
+        println!("Mode: {:?}", state.id_to_mode);
         // TODO: Notify that the outputs are done.
     }
 
-    event_created_child!(AppData, zwlr_output_head_v1::ZwlrOutputHeadV1, [
-       zwlr_output_manager_v1::EVT_HEAD_OPCODE => (zwlr_output_head_v1::ZwlrOutputHeadV1, ()),
+    event_created_child!(AppData, ZwlrOutputHeadV1, [
+       zwlr_output_manager_v1::EVT_HEAD_OPCODE => (ZwlrOutputHeadV1, ()),
     ]);
 }
 
-impl Dispatch<zwlr_output_head_v1::ZwlrOutputHeadV1, ()> for AppData {
+impl Dispatch<ZwlrOutputHeadV1, ()> for AppData {
     fn event(
-        _state: &mut Self,
-        _proxy: &zwlr_output_head_v1::ZwlrOutputHeadV1,
+        state: &mut Self,
+        _proxy: &ZwlrOutputHeadV1,
         event: zwlr_output_head_v1::Event,
         _data: &(),
         _conn: &Connection,
         _qhandle: &wayland_client::QueueHandle<Self>,
     ) {
-        dbg!(event);
+        match event {
+            zwlr_output_head_v1::Event::Mode { mode } => {
+                state
+                    .id_to_partial_mode
+                    .insert(mode.id(), PartialMode::default());
+            }
+            zwlr_output_head_v1::Event::CurrentMode { mode: _ } => {
+                // TODO: Mark this mode as current for this head.
+            }
+            _ => {}
+        }
     }
 
     event_created_child!(AppData, ZwlrOutputModeV1, [
-        zwlr_output_head_v1::EVT_CURRENT_MODE_OPCODE => (ZwlrOutputModeV1, ()),
         zwlr_output_head_v1::EVT_MODE_OPCODE => (ZwlrOutputModeV1, ()),
     ]);
 }
 
 impl Dispatch<ZwlrOutputModeV1, ()> for AppData {
     fn event(
-        _state: &mut Self,
-        _proxy: &ZwlrOutputModeV1,
-        _event: <ZwlrOutputModeV1 as wayland_client::Proxy>::Event,
+        state: &mut Self,
+        proxy: &ZwlrOutputModeV1,
+        event: zwlr_output_mode_v1::Event,
         _data: &(),
         _conn: &Connection,
         _qhandle: &wayland_client::QueueHandle<Self>,
     ) {
+        let id = proxy.id();
+        match event {
+            zwlr_output_mode_v1::Event::Size { width, height } => {
+                let partial_mode = state
+                    .id_to_partial_mode
+                    .get_mut(&id)
+                    .expect("The mode was previously reported and not finished.");
+                partial_mode.size = Some((width as u32, height as u32));
+            }
+            zwlr_output_mode_v1::Event::Refresh { refresh } => {
+                let partial_mode = state
+                    .id_to_partial_mode
+                    .get_mut(&id)
+                    .expect("The mode was previously reported and not finished.");
+                partial_mode.refresh = Some(refresh as u32);
+            }
+            zwlr_output_mode_v1::Event::Finished => {
+                state.id_to_partial_mode.remove(&id);
+                state.id_to_mode.remove(&id);
+                proxy.release();
+            }
+            _ => {}
+        }
     }
 }
